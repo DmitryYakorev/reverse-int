b2275e9682e54a2adbedfa8b4d4d005c6a7194c9
urakend@mail.ru <urakend@mail.ru>
2020-03-18 23:37:49 +0300

    feat:done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+  //  let Array = [];
+    //newArray = Array.map(n);
+    //console.log(newArray);
+    return Number(Math.abs(n).toString().split('').reverse().join(''));
 }
